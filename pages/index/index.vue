c3fc61f685daba5bb3322da2879538d516531208
huangsijia <hmc_cici@hotmail.com>
2019-05-23 17:41:51 +0800

    添加事件

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -5,22 +5,40 @@
 			<view class="top_view"></view>  
 		</view>  
 		<!-- #endif --> 
-		<h3>日记</h3>
+		<div class="title">
+			<h3>记事本</h3>
+			<image src="../../static/img/icon-add.png" class="icon" @click='add'></image>
+		</div>
 		<ul class="" v-for="(item,$index) in indexList" :key="item">
 			<li>
 				<p>{{$index+1}}：{{item}}</p>
-				<p class="date">{{date}}</p>
+				<div class="date">
+					<p>{{date}}</p>
+					<p class="finish" @click="finishFun">已完成</p>
+				</div>
 			</li>
 		</ul>
+		<lvv-popup position="bottom" ref="addLvvpopref">
+			<view class="lvvpopref addLvvpopref">
+				<textarea v-model="addTxt" :focus="addFocus" />	
+				<span class="addContent">添加</span>
+			</view>
+		</lvv-popup>
 	</view>
 </template>
 
 <script>
+	import lvvPopup from '../../components/lvv-popup/lvv-popup.vue'
 	import {mapState,mapMutations} from 'vuex'
 	export default {
+		components: {
+			lvvPopup
+		},
 		data() {
 			return {
-				date:""
+				date:"",
+				addTxt:"",
+				addFocus:false
 			}
 		},
 		onLoad() {
@@ -33,6 +51,15 @@
 		},
 		methods: {
 			...mapMutations(['login']),
+			add(){
+				this.$refs.addLvvpopref.show();
+				this.addFocus = true;
+			},
+			finishFun(){
+				uni.reLaunch({
+					url: "/pages/home/home"
+				})
+			}
 		},
 		mounted(){
 			var persion = uni.getStorageSync("persion");
@@ -58,18 +85,21 @@
 		background-color: #F8F8F8;  
 		top: 0;  
 		z-index: 999;  
-	}  
+	}
 	.index{
 		height: var(--status-bar-height);  
 		width: 100%;  
 		background: $bg;
+		.title{
+			display: flex;
+			justify-content: space-between;
+			background: $bg;
+			padding:0 24upx;
+			padding-bottom: 20upx;
+		}
 		h3{
-			height: 80upx;
-			line-height: 80upx;
 			color:$main;
 			font-size: 40upx;
-			padding:0 24upx;
-			background: $bg;
 		}
 		ul{
 			padding:0 24upx;
@@ -82,6 +112,32 @@
 			padding:24upx 0;
 			.date{
 				margin-left: 10upx;
+			}
+		}
+		.finish{
+			text-align: center;
+			text-decoration: underline;
+			color:$main;
+		}
+		.lvvpopref{
+			width: 100%;
+			background: #FFFFFF;
+			position: absolute;
+			bottom:0;
+			font-size: 30upx;
+			padding:24upx;
+			&.addLvvpopref{
+				min-height:120upx;
+				display: flex;
+			}
+			.addContent{
+				height: 60upx;
+				line-height: 60upx;
+				display: inline-block;
+				padding:0 24upx;
+				background: $main;
+				color:$white;
+				border-radius: 60upx;
 			}
 		}
 	}
